4a4630470afe51762fc31d568c2714ea1d6cae45
YumiLee <chn.lee.yumi@gmail.com>
2018-07-12 23:17:23 +0800

    更新到v1.2.0
    - v1.1.0 新增视频（最近72小时中国区视频、最近72小时全圆盘视频），来自国家卫星气象中心。
    - v1.2.0 新增动图，FY4A真彩色。图片列表来自中央气象台。

---
+++ index/gif.js
@@ -0,0 +1,102 @@
+const app = getApp()
+const hotapp = require('../utils/hotapp.js')
+
+//TODO: 播放功能按云图时间间隔来设定间隔（FY4A的图片拍照间隔不是相同的……不懂为啥）
+
+Page({
+  data: {
+    imgsrc: "",
+    img_load_complete: false,
+    img_name: "FY4A真彩色",
+    img_num: 0,
+    img_json: '',
+    img_num_max: 11,
+    is_playing: false
+  },
+
+  onLoad: function() {
+    const FY4A = "http://www.nmc.cn/rest/category/d3236549863e453aab0ccc4027105bad"
+    var that = this
+    hotapp.request({
+      useProxy: true,
+      url: FY4A,
+      success: function(res) {
+        that.setData({
+          img_json: res.data,
+          img_num_max: res.data.dataList.length,
+          img_num: res.data.dataList.length
+        })
+        that.refresh()
+      }
+    })
+  },
+
+  onShareAppMessage(options) {
+    return {
+      title: '台风来了吗？看看云图吧！',
+      path: '/index/index'
+    }
+  },
+
+  imageLoad: function(event) {
+    this.setData({
+      img_load_complete: true
+    })
+    //console.log(event)
+  },
+
+  doSetTimeout: function(i) {
+    var that = this
+    setTimeout(function() {
+      console.log(i);
+      that.setData({
+        img_num: i
+      })
+      that.refresh()
+      if (i == that.data.img_num_max) {
+        that.setData({
+          is_playing: false
+        })
+      }
+    }, 500 * i);
+  },
+
+  playFY4A: function(event) {
+    console.log("Play FY4A")
+    for (var i = 1; i <= this.data.img_num_max; ++i)
+      this.doSetTimeout(i);
+    this.setData({
+      is_playing: true
+    })
+  },
+
+  changeFY4A: function(event) {
+    console.log(event.detail.value)
+    this.setData({
+      img_num: event.detail.value
+    })
+    this.refresh()
+  },
+
+  imageTap: function(event) {
+    var full_imgurl = []
+    for (var i = this.data.img_num_max - 1; i >= 0; i--) {
+      full_imgurl.push("http://image.nmc.cn" + this.data.img_json.dataList[i].imgPath.replace(/\/medium/, ""))
+    }
+    //console.log(full_imgurl)
+    //console.log(this.data.img_num)
+    wx.previewImage({
+      current: full_imgurl[this.data.img_num - 1],
+      urls: full_imgurl
+    })
+  },
+
+  refresh: function(event) {
+    //console.log(this.data.img_json.dataList)
+    //console.log(this.data.img_num)
+    var imgurl = this.data.img_json.dataList[this.data.img_num_max - this.data.img_num].imgPath
+    this.setData({
+      imgsrc: "http://image.nmc.cn" + imgurl
+    })
+  },
+})
--- index/index.js
+++ index/index.js
@@ -4,62 +4,61 @@ const hotapp = require('../utils/hotapp.js')
 
 
 /*TODO:
-3. 接入中央气象台其它云图（不要求能播放了）
-1. 实现图像列表播放
-2. 实现更多图像列表（基于图片命名规律）
-4. 接入葵花8云图
+1. 接入葵花8云图
 */
 
 Page({
   data: {
     imgsrc: "",
     img_load_complete: false,
-    img_name: "FY4A真彩色"
+    img_name: "FY4A中国区高清",
+    time: 0
   },
 
-  onLoad: function() {
+  onLoad: function () {
+    this.setData({
+      time: Math.round(new Date().getTime() / 600000)//10min
+    })
     this.refresh()
   },
 
-  imageLoad: function(event) {
+  imageLoad: function (event) {
     this.setData({
       img_load_complete: true
     })
-    console.log(event)
+    //console.log(event)
   },
 
-  openList: function() {
+  openList: function () {
     var that = this
+    //var itemList = ['FY4A真彩色', 'FY2E彩色云图', 'FY4A中国区高清', 'FY4A圆盘图高清', '更多卫星云图适配中……']
+    var itemList = ['FY4A中国区高清', 'FY4A圆盘图高清', 'FY2E彩色云图', '更多卫星云图适配中……']
     wx.showActionSheet({
-      itemList: ['FY4A真彩色', 'FY2E彩色云图', '开发中……'],
-      success: function(res) {
+      itemList: itemList,
+      success: function (res) {
         if (!res.cancel) {
-          switch (res.tapIndex) {
-            case 0:
-              that.setData({
-                img_name: "FY4A真彩色"
-              })
-              break;
-            case 1:
-              that.setData({
-                img_name: "FY2E彩色云图"
-              })
-              break;
-            default:
-              that.setData({
-                img_name: "FY4A真彩色"
-              })
-          }
+          that.setData({
+            img_name: itemList[res.tapIndex],
+            img_num: 0
+          })
           that.refresh()
         }
       }
     });
   },
 
+  onShareAppMessage(options) {
+    return {
+      title: '台风来了吗？看看云图吧！',
+      path: '/index/index'
+    }
+  },
 
 
 
-  imageTap: function(event) {
+
+
+  imageTap: function (event) {
     switch (this.data.img_name) {
       case "FY4A真彩色":
       case "FY2E彩色云图":
@@ -69,18 +68,26 @@ Page({
           urls: [full_imgurl] // 需要预览的图片http链接列表
         })
         break;
+      case "FY4A中国区高清":
+      case "FY4A圆盘图高清":
+        wx.previewImage({
+          current: this.data.imgsrc + "?v=" + this.data.time, // 当前显示图片的http链接
+          urls: [this.data.imgsrc + "?v=" + this.data.time] // 需要预览的图片http链接列表
+        })
       default:
 
     }
-    
+
   },
 
-  refresh: function(event) {
+  refresh: function (event) {
     //云图列表接口
-    const FY4A = "http://www.nmc.cn/rest/category/d3236549863e453aab0ccc4027105bad"//从这里找到的：http://wx.nmc.cn/f/page/5fa5e41b6552474b91f95475fb5ad893/weatherMonitor/satellite
-    const FY2E = 'http://www.nmc.cn/f/rest/relevant/52'//从这里找到的：http://www.nmc.gov.cn/publish/satellite/fy2.htm
+    const FY4A = "http://www.nmc.cn/rest/category/d3236549863e453aab0ccc4027105bad" //从这里找到的：http://wx.nmc.cn/f/page/5fa5e41b6552474b91f95475fb5ad893/weatherMonitor/satellite
+    const FY2E = 'http://www.nmc.cn/f/rest/relevant/52' //从这里找到的：http://www.nmc.gov.cn/publish/satellite/fy2.htm
+    const FY4A_big = 'http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/FY4A_CHINA.JPG'
+    const FY4A_circle_big = 'http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/FY4A_DISK.JPG'
     var that = this
-    this.setData({//显示加载中
+    this.setData({ //显示加载中
       img_load_complete: false
     })
     //*****云图适配器*****
@@ -103,16 +110,26 @@ Page({
           useProxy: true,
           url: FY2E,
           success: function (res) {
-            console.log(res.data)
+            //console.log(res.data)
             var imgurl = res.data[0].imgPath
-            that.setData({ 
+            that.setData({
               imgsrc: "http://image.nmc.cn" + imgurl
             })
           }
         })
         break;
+      case "FY4A中国区高清":
+        that.setData({
+          imgsrc: FY4A_big + "?v=" + this.data.time
+        })
+        break;
+      case "FY4A圆盘图高清":
+        that.setData({
+          imgsrc: FY4A_circle_big + "?v=" + this.data.time
+        })
+        break;
       default:
-        
+
     }
   }
 })
